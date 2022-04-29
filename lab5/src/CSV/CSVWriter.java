8caec2878d66f82b649c17c3248fb5a9ce3756dc
LIN020504 <79856214+LIN020504@users.noreply.github.com>
2022-04-29 21:44:48 +0300

    update

--- lab5/src/CSV/CSVWriter.java
+++ lab5/src/CSV/CSVWriter.java
@@ -2,8 +2,10 @@ package CSV;
 
 import collection.Person;
 import collection.Ticket;
+import tools.tools;
 
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 
@@ -20,6 +22,25 @@ public class CSVWriter {
      */
     public void WriterToFile(LinkedHashSet<Ticket> linkedHashSet, String path) throws IOException{
         File file=new File(path);
+        if (!file.exists()) {
+            file.createNewFile();
+        } else{
+            tools.Message("Program: File Already exist, Would you like to replace it? Y/N:");
+            switch (tools.Input()){
+                case "Y" :{
+                    file.delete();
+                    file.createNewFile();
+                    break;
+                }
+                case "N" :{
+                    throw new FileAlreadyExistsException("Program: command return because file exist");
+                }
+                default: {
+                    tools.MessageL("Program: command return because input error!");
+                    break;
+                }
+            }
+        }
         String firstline="";
         BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
         String addfirstline="id,name,price,type,passport,height,creationdate\n";
